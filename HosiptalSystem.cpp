e9e4b297dae4780f0c800384cc1bfd018b7e205b
ahmed walled aref <ahmedaref858@gmail.com>
2026-04-29 06:38:22 +0300

    all functions of system done

--- HosiptalSystem.cpp
+++ HosiptalSystem.cpp
@@ -12,11 +12,11 @@ struct patient
 
 bool Add_Patient(patient specialization[20][5], int availaible_sets_in_specialization[20], int specialization_number, string Name, bool statis)
 {
-    specialization_number--;
     if (availaible_sets_in_specialization[specialization_number] < 5)
     {
         if (statis == 0)
         {
+            /*
             for (int i{0}; i < 5; i++)
             {
                 if (specialization[specialization_number][i].name == "")
@@ -24,6 +24,8 @@ bool Add_Patient(patient specialization[20][5], int availaible_sets_in_specializ
                     specialization[specialization_number][i] = {Name, statis};
                 }
             }
+            */
+            specialization[specialization_number][availaible_sets_in_specialization[specialization_number]] = {Name, statis};
         }
         else
         {
@@ -31,19 +33,68 @@ bool Add_Patient(patient specialization[20][5], int availaible_sets_in_specializ
             for (int i{4}; i > 0; i--)
             {
                 // do a shift only if not urgent
-                if (specialization[specialization_number][i-1].statis == 0)
+                if (specialization[specialization_number][i - 1].statis == 0)
                 {
                     specialization[specialization_number][i] = specialization[specialization_number][i - 1];
-                    
+                    // no urgent patients first urgent one
+                    if (i == 1)
+                    {
+                        specialization[specialization_number][0] = {Name, statis};
+                    }
                 }
-                else{
+                else
+                {
                     specialization[specialization_number][i] = {Name, statis};
+                    break;
                 }
             }
         }
         availaible_sets_in_specialization[specialization_number]++;
+        return true;
     }
     return false;
+}
+
+void Print_patients_in_specializtion(patient specialization[20][5], int availaible_sets_in_specialization[20],
+                                     int specialization_number)
+
+{
+    for (int i = 0; i < availaible_sets_in_specialization[specialization_number]; i++)
+    {
+        if (specialization[specialization_number][i].name != "")
+        {
+            cout << specialization[specialization_number][i].name << " ";
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << "\n";
+}
+
+void get_next_Patient(patient specialization[20][5], int availaible_sets_in_specialization[20], int specialization_number)
+{
+    if (availaible_sets_in_specialization[specialization_number] == 0)
+    {
+        cout << "Doctor, there are no patients.\n";
+    }
+    else
+    {
+        cout << specialization[specialization_number][0].name << " please go with the doctor\n";
+
+        int current_count = availaible_sets_in_specialization[specialization_number];
+        for (int i = 0; i < current_count - 1; i++)
+        {
+            specialization[specialization_number][i] = specialization[specialization_number][i + 1];
+        }
+
+        availaible_sets_in_specialization[specialization_number]--;
+
+        // 5. Clean up the "old" last position so it doesn't look like an urgent patient
+        int new_count = availaible_sets_in_specialization[specialization_number];
+        specialization[specialization_number][new_count] = {"", 0};
+    }
 }
 
 int main()
@@ -77,6 +128,7 @@ int main()
             cin >> specialization_num;
             cin >> Name;
             cin >> statis;
+            specialization_num--;
             bool is_added = Add_Patient(specialization_arr, availaible_sets_in_specialization, specialization_num, Name, statis);
             if (is_added)
             {
@@ -89,9 +141,19 @@ int main()
         }
         else if (user_coiche == 2)
         {
+            int specialization_num{0};
+            cout << "Enter specialization number that you want to list: \n";
+            cin >> specialization_num;
+            specialization_num--;
+            Print_patients_in_specializtion(specialization_arr, availaible_sets_in_specialization, specialization_num);
         }
         else if (user_coiche == 3)
         {
+            int specialization_num{0};
+            cout << "Enter specialization that you want to get patient from : \n";
+            cin >> specialization_num;
+            specialization_num--;
+            get_next_Patient(specialization_arr, availaible_sets_in_specialization, specialization_num);   
         }
         else if (user_coiche == 4)
         {
